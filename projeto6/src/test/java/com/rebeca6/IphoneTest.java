bbc76cf2d056d42968dbee5d69d0d0447776e993
Rebeca <becalbuquerque_23@hotmail.com>
2024-03-16 17:18:02 -0300

    adicionando teste com a notação @test

--- projeto6/src/test/java/com/rebeca6/IphoneTest.java
+++ projeto6/src/test/java/com/rebeca6/IphoneTest.java
@@ -1,26 +1,15 @@
 package com.rebeca6;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class IphoneTest{
 
-    @Test
-    void musicaTocando(){
     
-    Iphone iphone = new Iphone();
-    String musicaEsperada = "that´s life";
-    iphone.tocar(musicaEsperada);
-    Assertions.assertEquals(musicaEsperada, iphone.estaTocando());
-    
-    }
+@Test
 
-    @Test 
-    void deveAtualizarPagina(){
-     Iphone iphone = new Iphone();
-     iphone.atualizarPagina();
-     Assertions.assertTrue(iphone.estaAtualizada());
-    }
+
+   
+    
 
 
 
