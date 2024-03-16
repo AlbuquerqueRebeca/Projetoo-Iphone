d47933b495e37b8e8cee87b390d37a625babf468
Rebeca <becalbuquerque_23@hotmail.com>
2024-03-16 18:48:36 -0300

    chamando o metodo ligar

--- projeto6/src/test/java/com/rebeca6/IphoneTest.java
+++ projeto6/src/test/java/com/rebeca6/IphoneTest.java
@@ -27,7 +27,9 @@ Assertions.assertEquals(bandaEsperada, iphone.bandaTocando());
 @Test
 void ligandoParaContato(){
  Iphone iphone = new Iphone(); 
- String ligaçaoEsperada = "123456";  
+ String ligaçaoEsperada = "123456"; 
+ iphone.ligar(ligaçaoEsperada);
+  
 }
 
 
