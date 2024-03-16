261c10117f058c556259cc66a8a9b8ea1d8cb5a9
Rebeca <becalbuquerque_23@hotmail.com>
2024-03-16 18:42:30 -0300

    iniciando teste com a notaçao @test

--- projeto6/src/test/java/com/rebeca6/IphoneTest.java
+++ projeto6/src/test/java/com/rebeca6/IphoneTest.java
@@ -25,12 +25,6 @@ Assertions.assertEquals(bandaEsperada, iphone.bandaTocando());
 
  
 @Test
-void ligandoParaContato(){
- Iphone iphone = new Iphone();
- String ligaçaoEsperada = "123456";
- iphone.ligar(ligaçaoEsperada);
- Assertions.assertEquals(ligaçaoEsperada, iphone.estaLigando());   
-}
 
 
 
